bd55971ba6278cdbaf64b7779ebea3ea80844542
Juliusz Ceasar <marcin@o2.pl>
2024-01-31 03:06:39 +0100

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var list = Enumerable.Range(0, 50).Select(x => new RowModel(
+                $"{1}:{x}",
+                $"{2}:{x}",
+                $"{3}:{x}",
+                $"{4}:{x}",
+                $"{5}:{x}"));
+
+
+            var reportDS = new ReportDataSource("DataSet1", list);
+            reportViewer1.LocalReport.ReportPath = "Report1.rdlc";
+            reportViewer1.LocalReport.DataSources.Add(reportDS);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ WindowsFormsApp3/RowModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    public class RowModel
+    {
+        public string Col1 { get; set; }
+        public string Col2 { get; set; }
+        public string Col3 { get; set; }
+        public string Col4 { get; set; }
+        public string Col5 { get; set; }
+
+
+        public RowModel(string col1, string col2, string col3, string col4, string col5)
+        {
+            Col1 = col1;
+            Col2 = col2;
+            Col3 = col3;
+            Col4 = col4;
+            Col5 = col5;
+        }
+    }
+}
